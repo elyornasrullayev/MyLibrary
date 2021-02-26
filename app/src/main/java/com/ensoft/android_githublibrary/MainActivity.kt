65db4c3420a6c96d1c2a5be26fcbddd335fa9451
elyor <elyor.nasrullayev1999yil@gmail.com>
2021-02-26 22:47:30 +0500

    First commit

---
+++ app/src/main/java/com/ensoft/android_githublibrary/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ensoft.android_githublibrary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ensoft.mylibrary.Hisoblash
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
